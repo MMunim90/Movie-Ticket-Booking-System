b704077425852ade6a95200304b4434d030cd7c4
MMunim <mmunim9.9.01@gmail.com>
2026-05-13 00:33:47 +0600

    working on view available seats method

--- hall.py
+++ hall.py
@@ -10,3 +10,24 @@ class Hall:
     def enter_show(self, show_id, movie_name, time):
         self.tuple = (show_id, movie_name, time)
         self.show_list.append(self.tuple)
+        self.seat_list = []
+        for i in range(self.row):
+            row = []
+            for j in range(self.col):
+                row.append('free')
+            self.seat_list.append(row)
+        self.seats[show_id] = self.seat_list
+        
+    def view_show_list(self):
+        for x in self.show_list:
+            print(f"Show id: {x[0]}, \t Movie: {x[1]}, \t Time: {x[2]}")
+            
+    def view_available_seats(self, id):
+        for i in self.show_list:
+            if i[0] == id:
+                print(f"Movie: {i[1]}, \t\t Time: Today at {i[2]}\n")
+                
+        for x in range(self.row):
+            for y in range(self.col):
+                print(self.seats[id][x][y], end = " ")
+            print("\n")
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ my_hall.enter_show(321, 'Hati', '07:00 PM')
 my_hall.enter_show(345, 'Gora', '10:00 PM')
 
 while True:
-    print("\n-----------------------------------------\n")
+    print("\n--------------------------------------------------------\n")
     print("1. View all show today.")
     print("2. View available seats.")
     print("3. Book tickets.")
@@ -18,13 +18,18 @@ while True:
 
     option = int(input("Enter your option: "))
     if option == 1:
-        pass
+        print("\n--------------------------------------------------------\n")
+        my_hall.view_show_list()
     elif option == 2:
-        pass
+        id = int(input("Enter Show ID: "))
+        print("\n--------------------------------------------------------\n")
+        my_hall.view_available_seats(id)
     elif option == 3:
         pass
     elif option == 4:
         print("Please wait.....")
+        sleep(1)
+        print("Shutting down.....")
         sleep(random.randint(2,3))
         break
     else:
