7880c7118db80e87ffa23982bf50b75a7351f0d2
MMunim <mmunim9.9.01@gmail.com>
2026-05-13 06:31:19 +0600

    handle a corner case

--- hall.py
+++ hall.py
@@ -23,14 +23,17 @@ class Hall:
             print(f"Show id: {x[0]}, \t Movie: {x[1]}, \t Time: {x[2]}")
             
     def view_available_seats(self, id):
-        for i in self.show_list:
-            if i[0] == id:
-                print(f"Movie: {i[1]} \t\t Time: Today at {i[2]}\n\n")
-                
-        for x in range(self.row):
-            for y in range(self.col):
-                if(self.seats[id][x][y] == 'free'):
-                    print(f'{chr(x+65)}{y+1}', end = "\t")
-                else:
-                    print("X", end = "\t")
-            print("\n")
+        if id not in self.seats:
+            print("Wrong Show ID")
+        else:
+            for i in self.show_list:
+                if i[0] == id:
+                    print(f"Movie: {i[1]} \t\t Time: Today at {i[2]}\n\n")
+                    
+            for x in range(self.row):
+                for y in range(self.col):
+                    if(self.seats[id][x][y] == 'free'):
+                        print(f'{chr(x+65)}{y+1}', end = "\t")
+                    else:
+                        print("X", end = "\t")
+                print("\n")
